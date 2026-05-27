9d3487b51da2ea2de617d0e8744efbd586735702
Bani <bhansky.pake23@gmail.com>
2026-05-27 20:39:21 +0700

    Struktur rapi semua project: PDF di Dokumentasi, ZIP di Data, Code di Code, portfolio PDF di root

--- Project_02_Household_and_Item_Request_Management_System_Laravel/02_Code/03af5b9738c3f6585f9adb6f421e6e9d.php
+++
@@ -1,4 +0,0 @@
-<div
-  @click="sidebarToggle = false"
-  :class="sidebarToggle ? 'block lg:hidden' : 'hidden'"
-  class="fixed w-full h-screen z-9 bg-gray-900/50"></div><?php /**PATH E:\RUMGABPSDMIMIPAS\RUMGABPSDMIMIPAS\resources\views/user/partials/overlay.blade.php ENDPATH**/ ?>
--- Project_02_Household_and_Item_Request_Management_System_Laravel/02_Code/1849417b421f66ef31c7013ff7c44bf1.php
+++
@@ -1,4 +0,0 @@
-<div
-  @click="sidebarToggle = false"
-  :class="sidebarToggle ? 'block lg:hidden' : 'hidden'"
-  class="fixed w-full h-screen z-9 bg-gray-900/50"></div><?php /**PATH /home/mclarens-username/Downloads/BMN-VS2/resources/views/user/partials/overlay.blade.php ENDPATH**/ ?>
